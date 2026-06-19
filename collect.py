54dff23ec080e1f0a861ed75807e6ea2f82c3ca9
chperenniora <chperenniora@gmail.com>
2026-06-20 01:01:10 +0900

    Update collect.py

--- collect.py
+++ collect.py
@@ -1,1 +1,10 @@
+import json
 
+data = [
+    {"title": "시스템 정상 작동 테스트", "place": "확인 완료", "date": "오늘"}
+]
+
+with open('events.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
+
+print("파일 생성 성공")
